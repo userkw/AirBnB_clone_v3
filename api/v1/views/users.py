6cadaac602c2e86d9de4455902e51a59001c45f7
userkw <elkis.kawthar11@gmail.com>
2024-06-02 09:22:52 -0700

    done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,69 +1,40 @@
-#!/usr/bin/python3
-"""State objects that handles all default RESTFul API actions"""
+import hashlib
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from api.v1.views import app_views
-from models import storage
-from models.user import User
-from flask import abort, request, jsonify
-
-
-@app_views.route("/users", strict_slashes=False, methods=["GET"])
-@app_views.route("/users/<user_id>", strict_slashes=False,
-                 methods=["GET"])
-def user(user_id=None):
-    """show user and user with id"""
-    user_list = []
-    if user_id is None:
-        all_objs = storage.all(User).values()
-        for v in all_objs:
-            user_list.append(v.to_dict())
-        return jsonify(user_list)
+class User(BaseModel, Base):
+    """Representation of a user"""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all,delete", backref="user")
+        reviews = relationship("Review", cascade="all,delete", backref="user")
     else:
-        result = storage.get(User, user_id)
-        if result is None:
-            abort(404)
-        return jsonify(result.to_dict())
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
+    def __init__(self, *args, **kwargs):
+        """Initializes user"""
+        super().__init__(*args, **kwargs)
 
-@app_views.route("/users/<user_id>", strict_slashes=False,
-                 methods=["DELETE"])
-def user_delete(user_id):
-    """delete method"""
-    obj = storage.get(User, user_id)
-    if obj is None:
-        abort(404)
-    storage.delete(obj)
-    storage.save()
-    return jsonify({}), 200
+    def update_password(self, new_password):
+        """
+        Update the user's password and hash it using MD5.
+        """
+        self.password = hashlib.md5(new_password.encode()).hexdigest()
 
-
-@app_views.route("/users", strict_slashes=False, methods=["POST"])
-def create_user():
-    """creates a new post req"""
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    if "email" not in data:
-        abort(400, "Missing email")
-    if "password" not in data:
-        abort(400, "Missing password")
-    new_user = User(**data)
-    new_user.save()
-    return jsonify(new_user.to_dict()), 201
-
-
-@app_views.route("/users/<user_id>", strict_slashes=False,
-                 methods=["PUT"])
-def update_user(user_id):
-    """update user"""
-    obj = storage.get(User, user_id)
-    if obj is None:
-        abort(404)
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    obj.password = data.get("password", obj.password)
-    obj.first_name = data.get("first_name", obj.first_name)
-    obj.last_name = data.get("last_name", obj.last_name)
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+    def to_dict(self, save_to_disk=False):
+        """
+        Returns a dictionary representation of the User instance.
+        If save_to_disk is True, include the password key.
+        """
+        user_dict = super().to_dict()
+        if not save_to_disk:
+            user_dict.pop('password', None)  # Remove password key
+        return user_dict

baeb12938bca6e911dbea5d76490df79926d7896
userkw <elkis.kawthar11@gmail.com>
2024-06-02 09:26:05 -0700

    done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,10 +1,15 @@
-import hashlib
+#!/usr/bin/python3
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
-    """Representation of a user"""
+    """Representation of a user """
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -20,21 +25,5 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes user"""
+        """initializes user"""
         super().__init__(*args, **kwargs)
-
-    def update_password(self, new_password):
-        """
-        Update the user's password and hash it using MD5.
-        """
-        self.password = hashlib.md5(new_password.encode()).hexdigest()
-
-    def to_dict(self, save_to_disk=False):
-        """
-        Returns a dictionary representation of the User instance.
-        If save_to_disk is True, include the password key.
-        """
-        user_dict = super().to_dict()
-        if not save_to_disk:
-            user_dict.pop('password', None)  # Remove password key
-        return user_dict

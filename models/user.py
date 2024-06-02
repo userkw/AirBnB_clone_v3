41da0992f044c096f4f78284f4750e66ea13c771
userkw <elkis.kawthar11@gmail.com>
2024-06-02 09:34:40 -0700

    done

--- models/user.py
+++ models/user.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-""" holds class User"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import hashlib
 
 class User(BaseModel, Base):
     """Representation of a user """
@@ -25,5 +23,16 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """Initializes user"""
+        if kwargs.get('password'):
+            # Hash the password before storing
+            kwargs['password'] = hashlib.md5(kwargs['password'].encode()).hexdigest()
         super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance"""
+        new_dict = super().to_dict()
+        if models.storage_t != 'db':
+            # Remove password from the dictionary if not used by FileStorage
+            new_dict.pop('password', None)
+        return new_dict
